0f78fd892ab78b8a318e814ac3872f46b6356754
Gordon <gordondeng@foxmail.com>
2020-02-05 23:37:53 +0800

    back  to tit

---
+++ 148.排序链表.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=cpp
+ *
+ * [148] 排序链表
+ *
+ * https://leetcode-cn.com/problems/sort-list/description/
+ *
+ * algorithms
+ * Medium (63.04%)
+ * Likes:    392
+ * Dislikes: 0
+ * Total Accepted:    38.5K
+ * Total Submissions: 60.6K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode dummyHead(0);
+        dummyHead.next = head;
+        auto p = head;
+        int length = 0;
+        while (p) {
+            ++length;
+            p = p->next;
+        }
+        
+        for (int size = 1; size < length; size <<= 1) {
+            auto cur = dummyHead.next;
+            auto tail = &dummyHead;
+            
+            while (cur) {
+                auto left = cur;
+                auto right = cut(left, size); // left->@->@ right->@->@->@...
+                cur = cut(right, size); // left->@->@ right->@->@  cur->@->...
+                
+                tail->next = merge(left, right);
+                while (tail->next) {
+                    tail = tail->next;
+                }
+            }
+        }
+        return dummyHead.next;
+    }
+    
+    ListNode* cut(ListNode* head, int n) {
+        auto p = head;
+        while (--n && p) {
+            p = p->next;
+        }
+        
+        if (!p) return nullptr;
+        
+        auto next = p->next;
+        p->next = nullptr;
+        return next;
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummyHead(0);
+        auto p = &dummyHead;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                p = l1;
+                l1 = l1->next;       
+            } else {
+                p->next = l2;
+                p = l2;
+                l2 = l2->next;
+            }
+        }
+        p->next = l1 ? l1 : l2;
+        return dummyHead.next;
+    }
+};
+// @lc code=end
+
