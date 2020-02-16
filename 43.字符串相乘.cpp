0b6121f003d125df76639d9510cce5e982716cda
Gordon <gordondeng@foxmail.com>
2020-02-16 23:37:15 +0800

    back

---
+++ 43.字符串相乘.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=43 lang=cpp
+ *
+ * [43] 字符串相乘
+ *
+ * https://leetcode-cn.com/problems/multiply-strings/description/
+ *
+ * algorithms
+ * Medium (41.17%)
+ * Likes:    264
+ * Dislikes: 0
+ * Total Accepted:    43.2K
+ * Total Submissions: 103.9K
+ * Testcase Example:  '"2"\n"3"'
+ *
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ * 
+ * 示例 1:
+ * 
+ * 输入: num1 = "2", num2 = "3"
+ * 输出: "6"
+ * 
+ * 示例 2:
+ * 
+ * 输入: num1 = "123", num2 = "456"
+ * 输出: "56088"
+ * 
+ * 说明：
+ * 
+ * 
+ * num1 和 num2 的长度小于110。
+ * num1 和 num2 只包含数字 0-9。
+ * num1 和 num2 均不以零开头，除非是数字 0 本身。
+ * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ * 
+ * 
+ */
+
+// @lc code=start
+
+// 技巧题
+// T:O(m*n) S:O()
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n1=num1.size();
+        int n2=num2.size();
+        string res(n1+n2,'0');
+        for(int i=n2-1;i>=0;i--){
+            for(int j=n1-1;j>=0;j--){
+                int temp=(res[i+j+1]-'0')+(num1[j]-'0')*(num2[i]-'0');
+                res[i+j+1]=temp%10+'0';//当前位
+                res[i+j]+=temp/10; //前一位加上进位，res[i+j]已经初始化为'0'，加上int类型自动转化为char，所以此处不加'0'
+            }
+        }
+        
+//去除首位'0'
+        for(int i=0;i<n1+n2;i++){
+            if(res[i]!='0')
+                return res.substr(i);
+        }
+        return "0";
+       
+        
+    }
+};
+// @lc code=end
+
