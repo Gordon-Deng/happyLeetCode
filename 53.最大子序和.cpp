6dd272a4411927d0a78379916adde2b45155939e
Gordon <gordondeng@foxmail.com>
2020-02-20 01:19:38 +0800

    back

--- 53.最大子序和.cpp
+++ 53.最大子序和.cpp
@@ -29,9 +29,12 @@
  */
 
 // @lc code=start
+
+// T:O(N) S:O(1)
 class Solution
 {
 public:
+
     int maxSubArray(vector<int> &nums)
     {
         //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
@@ -52,5 +55,28 @@ public:
         return result;
     }
 };
+
+// T:O(n) S:O(1)
+// DP
+// class Solution
+// {
+// public:
+//     int maxSubArray(vector<int> &nums)
+//     {
+//         //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+//         int result = INT_MIN;
+//         int numsSize = int(nums.size());
+//         //因为只需要知道dp的前一项，我们用int代替一维数组
+//         int dp(nums[0]);
+//         result = dp;
+//         for (int i = 1; i < numsSize; i++)
+//         {
+//             dp = max(dp + nums[i], nums[i]);
+//             result = max(result, dp);
+//         }
+
+//         return result;
+//     }
+// };
 // @lc code=end
 
