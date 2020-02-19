5184d3f261ac6102186086b1e33091c60b400096
Gordon <gordondeng@foxmail.com>
2020-02-20 00:14:14 +0800

    back

---
+++ 53.最大子序和.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=cpp
+ *
+ * [53] 最大子序和
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (48.60%)
+ * Likes:    1630
+ * Dislikes: 0
+ * Total Accepted:    161.2K
+ * Total Submissions: 327.5K
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 
+ * 示例:
+ * 
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
+        int result = INT_MIN;
+        int numsSize = int(nums.size());
+        int sum = 0;
+        for (int i = 0; i < numsSize; i++)
+        {
+            sum += nums[i];
+            result = max(result, sum);
+            //如果sum < 0，重新开始找子序串
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
