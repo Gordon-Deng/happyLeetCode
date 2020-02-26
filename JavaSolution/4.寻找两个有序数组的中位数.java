2f5c88181ee34e6e685a0b5a84092ad2ed5bac26
Gordon <gordondeng@foxmail.com>
2020-02-27 04:57:53 +0800

    topK大法好

--- JavaSolution/4.寻找两个有序数组的中位数.java
+++ JavaSolution/4.寻找两个有序数组的中位数.java
@@ -89,49 +89,74 @@
 *      ③.A[i-1] > B[j]。此时应该把i减小。 即imax = i - 1;
 *
 * */
+// https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
 class Solution {
-    public double findMedianSortedArrays(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
-        if (m > n) { // to ensure m<=n
-            int[] temp = A; A = B; B = temp;
-            int tmp = m; m = n; n = tmp;
-        }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
-        while (iMin <= iMax) {
-            int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && B[j - 1] > A[i]) {
-                iMin = i + 1; // i is too small
-            } else if (i > iMin && A[i - 1] > B[j]) {
-                iMax = i - 1; // i is too big
-            } else { // i is perfect
-                int maxLeft;
-                if (i == 0) {//A分成的leftA(空集) 和 rightA(A的全部)  所以leftPart = leftA(空集) + leftB,故maxLeft = B[j-1]。
-                    maxLeft = B[j - 1];
-                } else if (j == 0) { //B分成的leftB(空集) 和 rightB(B的全部)  所以leftPart = leftA + leftB(空集),故maxLeft = A[i-1]。
-                    maxLeft = A[i - 1];
-                } else { //排除上述两种特殊情况，正常比较
-                    maxLeft = Math.max(A[i - 1], B[j - 1]);
-                }
-                if ((m + n) % 2 == 1) { //奇数，中位数正好是maxLeft
-                    return maxLeft;
-                }
-                //偶数
-                int minRight;
-                if (i == m) {//A分成的leftA(A的全部) 和 rightA(空集)  所以rightPart = rightA(空集) + rightB,故minRight = B[j]。
-                    minRight = B[j];
-                } else if (j == n) {//B分成的leftB(B的全部) 和 rightB(空集)  所以rightPart = rightA + rightB(空集),故minRight = A[i]。
-                    minRight = A[i];
-                } else {//排除上述两种特殊情况，正常比较
-                    minRight = Math.min(B[j], A[i]);
-                }
+    // public double findMedianSortedArrays(int[] A, int[] B) {
+    // 二分太难理解
+    // int m = A.length;
+    // int n = B.length;
+    // if (m > n) {
+    // return findMedianSortedArrays(B,A); // 保证 m <= n
+    // }
+    // int iMin = 0, iMax = m;
+    // while (iMin <= iMax) {
+    // int i = (iMin + iMax) / 2;
+    // int j = (m + n + 1) / 2 - i;
+    // if (j != 0 && i != m && B[j-1] > A[i]){ // i 需要增大
+    // iMin = i + 1;
+    // }
+    // else if (i != 0 && j != n && A[i-1] > B[j]) { // i 需要减小
+    // iMax = i - 1;
+    // }
+    // else { // 达到要求，并且将边界条件列出来单独考虑
+    // int maxLeft = 0;
+    // if (i == 0) { maxLeft = B[j-1]; }
+    // else if (j == 0) { maxLeft = A[i-1]; }
+    // else { maxLeft = Math.max(A[i-1], B[j-1]); }
+    // if ( (m + n) % 2 == 1 ) { return maxLeft; } // 奇数的话不需要考虑右半部分
 
-                return (maxLeft + minRight) / 2.0;
-            }
+    // int minRight = 0;
+    // if (i == m) { minRight = B[j]; }
+    // else if (j == n) { minRight = A[i]; }
+    // else { minRight = Math.min(B[j], A[i]); }
+
+    // return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+    // }
+    // }
+    // return 0.0;
+    // }
+
+    // 还是topk法好
+    // T:O(log(m+n)) S:O(1)
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        // 将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        // 让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2)
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0)
+            return nums2[start2 + k - 1];
+
+        if (k == 1)
+            return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        } else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
         }
-        return 0.0;
     }
 }
 // @lc code=end
-
