1c3f38fa33495a99fabc3e60852b4607121b4653
Gordon <gordondeng@foxmail.com>
2020-02-27 23:45:29 +0800

    back

---
+++ JavaSolution/5.最长回文子串.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (28.04%)
+ * Likes:    1622
+ * Dislikes: 0
+ * Total Accepted:    165.2K
+ * Total Submissions: 588K
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 
+ * 示例 1：
+ * 
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        
+    }
+}
+// @lc code=end
+
