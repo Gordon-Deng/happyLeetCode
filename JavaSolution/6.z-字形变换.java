697bc4aea9341f87a5a3722dcf5f46cf4e3efcbd
Gordon <gordondeng@foxmail.com>
2020-03-02 23:51:29 +0800

    6.ZigZag Conversion|n,n|模拟行索引的变化(先大再小再大))，在遍历s中把每个字符填到正确的行 res[i]

---
+++ JavaSolution/6.z-字形变换.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=java
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (46.15%)
+ * Likes:    523
+ * Dislikes: 0
+ * Total Accepted:    86.6K
+ * Total Submissions: 187.4K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * 
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * 
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 
+ * 
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ * 
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ * 
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ * 
+ */
+
+// @lc code=start
+
+// T:O(n)) S:O(n)
+// 模拟行索引的变化(先大再小再大))，在遍历s中把每个字符填到正确的行 res[i] 。
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
+    }
+}
+// @lc code=end
+
