ebb8dfcee13ba3402b96826715881d3501204330
Gordon <gordondeng@foxmail.com>
2020-03-04 02:54:11 +0800

    10.Regular Expression Matching|nm,nm|递归列举.和*的搭配

---
+++ JavaSolution/10.正则表达式匹配.java
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=java
+ *
+ * [10] 正则表达式匹配
+ *
+ * https://leetcode-cn.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.55%)
+ * Likes:    919
+ * Dislikes: 0
+ * Total Accepted:    50.5K
+ * Total Submissions: 194.8K
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * 
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 
+ * 
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * 
+ * 说明:
+ * 
+ * 
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+
+// 最好是动态规划T:O(nm) S:O(nm) nm分别为匹配串长度和模式串长度
+// 但这个是递归，比较好理解
+// T:O((n+m)*2^(n+m/2)) S:O()
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // 如果正则串p为空字符串s也为空这匹配成功，如果正则串p为空但是s不是空则说明匹配失败
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+        // 判断s和p的首字符是否匹配，注意要先判断s不为空
+        boolean headMatched = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        if (p.length() >= 2 && p.charAt(1) == '*') {// 如果p的第一个元素的下一个元素是*
+            // 则分别对两种情况进行判断
+            {
+                // * = 0,相当于p去两个字符
+                return isMatch(s, p.substring(2)) 
+                // * = 1或者更多，s先去
+                || (headMatched && isMatch(s.substring(1), p));
+            }
+        } else if (headMatched) {// 否则，如果s和p的首字符相等
+            return isMatch(s.substring(1), p.substring(1));
+        } else {
+            return false;
+        }
+    }
+}
+// @lc code=end
