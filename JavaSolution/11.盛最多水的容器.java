a7ce2260f1249d7fc593ac1f9eec2137622c23f6
Gordon <gordondeng@foxmail.com>
2020-03-05 02:40:41 +0800

    11.Container With Most Water|n,1|双指针法，向内移动，优先移动短的，为什么呢，因为面积受制于短的哪段

---
+++ JavaSolution/11.盛最多水的容器.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ *
+ * [11] 盛最多水的容器
+ *
+ * https://leetcode-cn.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (60.39%)
+ * Likes:    1046
+ * Dislikes: 0
+ * Total Accepted:    125.1K
+ * Total Submissions: 206.6K
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为
+ * (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ * 
+ * 
+ * 
+ * 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: [1,8,6,2,5,4,8,3,7]
+ * 输出: 49
+ * 
+ */
+
+// @lc code=start
+
+// T:O(N) S:O(1)
+// 双指针法，向内移动，优先移动短的，为什么呢，因为面积受制于短的哪段
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
+// @lc code=end
+
