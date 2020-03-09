72d82e8c5fafd31afab8ebbe97f334352c77155d
Gordon <gordondeng@foxmail.com>
2020-03-10 01:39:34 +0800

    16.3Sum Closest|n^2,1|先排序，固定首位，次位和尾位双指针夹

--- JavaSolution/16.最接近的三数之和.java
+++ JavaSolution/16.最接近的三数之和.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=16 lang=java
  *
@@ -24,9 +26,38 @@
  */
 
 // @lc code=start
+
+// T:O(n^2) S:O(1)
+// 先排序，固定首位，次位和尾位双指针夹
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left != right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(Math.abs(sum - target) < Math.abs(result - target))
+                    result = sum;
+                if(sum > target){
+                    right--;
+                    // 解决nums[right]重复
+                    while(left != right && nums[right] == nums[right+1])
+                        right--;
+                }
+                else{
+                    left++;
+                    // 解决nums[left]重复
+                    while(left != right && nums[left] == nums[left-1])
+                        left++;
+                }
+            }
+            // 解决nums[i]重复
+            while(i<nums.length-2 && nums[i] == nums[i+1])
+                i++;
+        }
+        return result;
     }
 }
 // @lc code=end
