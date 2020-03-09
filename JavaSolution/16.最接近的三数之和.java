2b5a98092f6d0fd22b8ba425c31bb9d0e8b984c7
Gordon <gordondeng@foxmail.com>
2020-03-10 01:47:58 +0800

    16.3Sum Closest|n^2,1|先排序，固定首位，次位和尾位双指针夹

--- JavaSolution/16.最接近的三数之和.java
+++ JavaSolution/16.最接近的三数之和.java
@@ -33,28 +33,27 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int result = nums[0] + nums[1] + nums[2];
-        for(int i=0;i<nums.length-2;i++){
-            int left = i+1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
             int right = nums.length - 1;
-            while(left != right){
+            while (left != right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(Math.abs(sum - target) < Math.abs(result - target))
+                if (Math.abs(sum - target) < Math.abs(result - target))
                     result = sum;
-                if(sum > target){
+                if (sum > target) {
                     right--;
                     // 解决nums[right]重复
-                    while(left != right && nums[right] == nums[right+1])
+                    while (left != right && nums[right] == nums[right + 1])
                         right--;
-                }
-                else{
+                } else {
                     left++;
                     // 解决nums[left]重复
-                    while(left != right && nums[left] == nums[left-1])
+                    while (left != right && nums[left] == nums[left - 1])
                         left++;
                 }
             }
             // 解决nums[i]重复
-            while(i<nums.length-2 && nums[i] == nums[i+1])
+            while (i < nums.length - 2 && nums[i] == nums[i + 1])
                 i++;
         }
         return result;
