9adfc61c10cc2a29cb8926be80eb394f88886b6b
Gordon <gordondeng@foxmail.com>
2020-03-10 23:57:15 +0800

    21.Merge Two Sorted Lists|N+M,1|

--- JavaSolution/21.合并两个有序链表.java
+++ JavaSolution/21.合并两个有序链表.java
@@ -33,23 +33,29 @@
  * }
  */
 
-// T:O(N+M) S:O(N+M)
+// T:O(N+M) S:O(1)
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null){
-            return l2;
+        // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
         }
-        if(l2 == null){
-            return l1;
-        }
-        if(l1.val < l2.val){
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        }
-        else{
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
     }
 }
 // @lc code=end
---
+++ JavaSolution/23.合并k个排序链表.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个排序链表
+ *
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (48.36%)
+ * Likes:    465
+ * Dislikes: 0
+ * Total Accepted:    68.8K
+ * Total Submissions: 141.9K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ * 
+ * 示例:
+ * 
+ * 输入:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 输出: 1->1->2->3->4->4->5->6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
+// @lc code=end
+
