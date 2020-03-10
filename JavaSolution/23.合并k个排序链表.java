bad40630dbfa0ddad6321a466e080252359b73c3
Gordon <gordondeng@foxmail.com>
2020-03-11 00:51:52 +0800

    23.Merge k Sorted Lists|k*log(n),1|合并双链表的基础上，两两归并1和k-1,2和k-2合并，不断循环到只有一组

--- JavaSolution/23.合并k个排序链表.java
+++ JavaSolution/23.合并k个排序链表.java
@@ -36,9 +36,46 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+ // T:O(N*log(k)) S:O()
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        
+        int len = lists.length;
+        if (len == 0) {
+            return null;
+        }
+        // 将n个链表以中间为对称，合并，即合并
+        while (len > 1) {
+            for (int i = 0; i < len / 2; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
+            }
+            len = (len + 1) / 2;
+        }
+        return lists[0];
+    }
+
+    // 合并两个链表
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
     }
 }
 // @lc code=end
