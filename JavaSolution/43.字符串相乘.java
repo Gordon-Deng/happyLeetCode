1edfa5bf0f0f07f94c3cf0f37b9a4ef0008a8325
Gordon <gordondeng@foxmail.com>
2020-03-13 00:19:33 +0800

    43.	 Multiply String|m*n,m+n|补0，并用大数相加法

---
+++ JavaSolution/43.字符串相乘.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=43 lang=java
+ *
+ * [43] 字符串相乘
+ *
+ * https://leetcode-cn.com/problems/multiply-strings/description/
+ *
+ * algorithms
+ * Medium (41.17%)
+ * Likes:    264
+ * Dislikes: 0
+ * Total Accepted:    43.2K
+ * Total Submissions: 103.9K
+ * Testcase Example:  '"2"\n"3"'
+ *
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ * 
+ * 示例 1:
+ * 
+ * 输入: num1 = "2", num2 = "3"
+ * 输出: "6"
+ * 
+ * 示例 2:
+ * 
+ * 输入: num1 = "123", num2 = "456"
+ * 输出: "56088"
+ * 
+ * 说明：
+ * 
+ * 
+ * num1 和 num2 的长度小于110。
+ * num1 和 num2 只包含数字 0-9。
+ * num1 和 num2 均不以零开头，除非是数字 0 本身。
+ * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ * 
+ * 
+ */
+
+// @lc code=start
+
+// T:O(m*n) S:O(n+m)
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        String res = "0";
+
+        for (int i = num2.length() - 1; i >= 0; i--) {
+            int carry = 0;
+            StringBuilder temp = new StringBuilder();
+            // 补 0, 重点在于I的取值是从最后往前减的，而J是从0，并不断计算两者的间距
+            for (int j = 0; j < num2.length() - 1 - i; j++) {
+                temp.append(0);
+            }
+            int n2 = num2.charAt(i) - '0';
+
+            // num2 的第 i 位数字 n2 与 num1 相乘
+            for (int j = num1.length() - 1; j >= 0 || carry != 0; j--) {
+                int n1 = j < 0 ? 0 : num1.charAt(j) - '0';
+                int product = (n1 * n2 + carry) % 10;
+                temp.append(product);
+                carry = (n1 * n2 + carry) / 10;
+            }
+            // 将当前结果与新计算的结果求和作为新的结果
+            res = addStrings(res, temp.reverse().toString());
+        }
+        return res;
+    }
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder("");
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int tmp = n1 + n2 + carry;
+            carry = tmp / 10;
+            res.append(tmp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1)
+            res.append(1);
+        return res.reverse().toString();
+    }
+}
+// @lc code=end
+
