b0c12a3be2c14fbc236fc6f234cab059e25b1552
Gordon <gordondeng@foxmail.com>
2020-03-13 23:53:43 +0800

    init

---
+++ JavaSolution/46.全排列.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=java
+ *
+ * [46] 全排列
+ *
+ * https://leetcode-cn.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (73.45%)
+ * Likes:    530
+ * Dislikes: 0
+ * Total Accepted:    77.2K
+ * Total Submissions: 104.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个没有重复数字的序列，返回其所有可能的全排列。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
