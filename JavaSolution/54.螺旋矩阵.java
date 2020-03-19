0449e00dafb6d1c28c51a6e90a2ad4d6bd7c31f6
Gordon <gordondeng@foxmail.com>
2020-03-19 23:32:41 +0800

    54.Spiral Matrix|N,N|遍历

---
+++ JavaSolution/54.螺旋矩阵.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=java
+ *
+ * [54] 螺旋矩阵
+ *
+ * https://leetcode-cn.com/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (38.08%)
+ * Likes:    288
+ * Dislikes: 0
+ * Total Accepted:    38.2K
+ * Total Submissions: 99.6K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 4, 5, 6 ],
+ * ⁠[ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * [
+ * ⁠ [1, 2, 3, 4],
+ * ⁠ [5, 6, 7, 8],
+ * ⁠ [9,10,11,12]
+ * ]
+ * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ * 
+ */
+
+// @lc code=start
+
+// T:O(N) S:O(N)
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = { 0, 1, 0, -1 };
+        int[] dc = { 1, 0, -1, 0 };
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]) {
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
