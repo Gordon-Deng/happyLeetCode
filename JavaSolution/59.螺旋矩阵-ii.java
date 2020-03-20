5366d01efbfb298e33016094c7b8d18d561e0028
Gordon <gordondeng@foxmail.com>
2020-03-20 23:45:03 +0800

    test

---
+++ JavaSolution/59.螺旋矩阵-ii.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=59 lang=java
+ *
+ * [59] 螺旋矩阵 II
+ *
+ * https://leetcode-cn.com/problems/spiral-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (75.99%)
+ * Likes:    139
+ * Dislikes: 0
+ * Total Accepted:    24.2K
+ * Total Submissions: 31.7K
+ * Testcase Example:  '3'
+ *
+ * 给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+ * 
+ * 示例:
+ * 
+ * 输入: 3
+ * 输出:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 8, 9, 4 ],
+ * ⁠[ 7, 6, 5 ]
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] generateMatrix(int n) {
+        
+    }
+}
+// @lc code=end
+
