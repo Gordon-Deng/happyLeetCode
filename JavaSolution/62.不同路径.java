59afaae311d711684ea9a3f8b356da2a29b15c60
Gordon <gordondeng@foxmail.com>
2020-03-24 01:04:18 +0800

    62.Unique Paths|M*N,N|dp

--- JavaSolution/62.不同路径.java
+++ JavaSolution/62.不同路径.java
@@ -44,10 +44,19 @@
  */
 
 // @lc code=start
+
+// T:O(M*N) S:O(N)
+// dp
 class Solution {
     public int uniquePaths(int m, int n) {
-        
+        int[] cur = new int[n];
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
     }
 }
 // @lc code=end
-
