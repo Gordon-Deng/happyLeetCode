384021e80dc1f715614ea90a2291b1bbf15faa3c
Gordon <gordondeng@foxmail.com>
2020-03-25 23:43:26 +0800

    78.subsets|n*2^n,n*2^n|回溯

---
+++ JavaSolution/78.子集.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ *
+ * https://leetcode-cn.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (75.86%)
+ * Likes:    463
+ * Dislikes: 0
+ * Total Accepted:    58.9K
+ * Total Submissions: 77.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+
+// @lc code=start
+
+// T:O(n*2^n) S:O(n*2^n)
+// https://leetcode-cn.com/problems/subsets/solution/hui-su-suan-fa-by-powcai-5/ 
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res, new ArrayList<Integer>());
+        return res;
+
+    }
+
+    private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
+        res.add(new ArrayList<>(tmp));
+        for (int j = i; j < nums.length; j++) {
+            tmp.add(nums[j]);
+            backtrack(j + 1, nums, res, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
+// @lc code=end
