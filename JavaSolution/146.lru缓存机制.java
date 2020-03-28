528080110c8b208a7ea1709d9fa2aa89c8d2e65d
Gordon <gordondeng@foxmail.com>
2020-03-28 23:20:06 +0800

    146.lru-cache|1,n|哈希表 + 双向链表

--- JavaSolution/146.lru缓存机制.java
+++ JavaSolution/146.lru缓存机制.java
@@ -26,14 +26,20 @@
  * 
  * 示例:
  * 
- * LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );**cache.put(1,1);*cache.put(2,2);*cache.get(1); // 返回  1
-*cache.put(3,3); // 该操作会使得密钥 2 作废
-*cache.get(2); // 返回 -1 (未找到)
-*cache.put(4,4); // 该操作会使得密钥 1 作废
-*cache.get(1); // 返回 -1 (未找到)
-*cache.get(3); // 返回  3
-*cache.get(4); // 返回  4
-***/
+ * LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+ * 
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // 返回  1
+ * cache.put(3, 3);    // 该操作会使得密钥 2 作废
+ * cache.get(2);       // 返回 -1 (未找到)
+ * cache.put(4, 4);    // 该操作会使得密钥 1 作废
+ * cache.get(1);       // 返回 -1 (未找到)
+ * cache.get(3);       // 返回  3
+ * cache.get(4);       // 返回  4
+ * 
+ * 
+ */
 
 // @lc code=start
 import java.util.Hashtable;
@@ -141,7 +147,6 @@ class LRUCache {
         }
     }
 }
-
 /**
  * Your LRUCache object will be instantiated and called as such: LRUCache obj =
  * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
