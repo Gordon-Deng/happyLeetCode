99bde3a87cac7f06071ff5815d4b34cb1ed59e6d
Gordon <gordondeng@foxmail.com>
2020-05-28 01:30:12 +0800

    122.best-time-to-buy-and-sell-stock-ii|N,1|寻峰法

--- JavaSolution/122.买卖股票的最佳时机-ii.java
+++ JavaSolution/122.买卖股票的最佳时机-ii.java
@@ -46,20 +46,15 @@
  */
 
 // @lc code=start
+
+// T:O(n) S:O(1)
+// 寻峰法优化，在谷之后使用叠加计算的方法计算利润，而不是寻找每一个峰值。
 class Solution {
     public int maxProfit(int[] prices) {
-        int i = 0;
-        int valley = prices[0];
-        int peak = prices[0];
         int maxprofit = 0;
-        while (i < prices.length - 1) {
-            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
-                i++;
-            valley = prices[i];
-            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
-                i++;
-            peak = prices[i];
-            maxprofit += peak - valley;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
         return maxprofit;
     }
