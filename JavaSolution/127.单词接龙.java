0ae93cefa1d8820498567eb04b4998f201b36a95
Gordon <gordondeng@foxmail.com>
2020-06-01 23:57:47 +0800

    127.word-ladder|M*N,M*N|双向BFS

---
+++ JavaSolution/127.单词接龙.java
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=127 lang=java
+ *
+ * [127] 单词接龙
+ *
+ * https://leetcode-cn.com/problems/word-ladder/description/
+ *
+ * algorithms
+ * Medium (38.78%)
+ * Likes:    203
+ * Dislikes: 0
+ * Total Accepted:    20.5K
+ * Total Submissions: 52.1K
+ * Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
+ *
+ * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord
+ * 的最短转换序列的长度。转换需遵循如下规则：
+ * 
+ * 
+ * 每次转换只能改变一个字母。
+ * 转换过程中的中间单词必须是字典中的单词。
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 如果不存在这样的转换序列，返回 0。
+ * 所有单词具有相同的长度。
+ * 所有单词只由小写字母组成。
+ * 字典中不存在重复的单词。
+ * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ * 
+ * 输出: 5
+ * 
+ * 解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ * ⁠    返回它的长度 5。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * beginWord = "hit"
+ * endWord = "cog"
+ * wordList = ["hot","dot","dog","lot","log"]
+ * 
+ * 输出: 0
+ * 
+ * 解释: endWord "cog" 不在字典中，所以无法进行转换。
+ * 
+ */
+
+// @lc code=start
+
+// https://leetcode-cn.com/problems/word-ladder/solution/suan-fa-shi-xian-he-you-hua-javashuang-xiang-bfs23/
+// T:O(mn) S:O(mn)
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+        
+        // 从两端BFS遍历要用的队列
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        // 两端已经遍历过的节点
+        Set<String> visited1 = new HashSet<>();
+        Set<String> visited2 = new HashSet<>();
+        queue1.offer(beginWord);
+        queue2.offer(endWord);
+        visited1.add(beginWord);
+        visited2.add(endWord);
+        
+        int count = 0;
+        Set<String> allWordSet = new HashSet<>(wordList);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count++;
+            if (queue1.size() > queue2.size()) {
+                Queue<String> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<String> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = queue1.poll();
+                char[] chars = s.toCharArray();
+                for (int j = 0; j < s.length(); ++j) {
+                    // 保存第j位的原始字符
+                    char c0 = chars[j];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        chars[j] = c;
+                        String newString = new String(chars);
+                        // 已经访问过了，跳过
+                        if (visited1.contains(newString)) {
+                            continue;
+                        }
+                        // 两端遍历相遇，结束遍历，返回count
+                        if (visited2.contains(newString)) {
+                            return count + 1;
+                        }
+                        // 如果单词在列表中存在，将其添加到队列，并标记为已访问
+                        if (allWordSet.contains(newString)) {
+                            queue1.offer(newString);
+                            visited1.add(newString);
+                        }
+                    }
+                    // 恢复第j位的原始字符
+                    chars[j] = c0;
+                }
+            }
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
