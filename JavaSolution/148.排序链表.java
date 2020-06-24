c737452fbd31e427881f414abd924f045fa4a456
Gordon <gordondeng@foxmail.com>
2020-06-25 02:30:53 +0800

    136.single-number|N,1|异或

---
+++ JavaSolution/148.排序链表.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=java
+ *
+ * [148] 排序链表
+ *
+ * https://leetcode-cn.com/problems/sort-list/description/
+ *
+ * algorithms
+ * Medium (63.04%)
+ * Likes:    392
+ * Dislikes: 0
+ * Total Accepted:    38.5K
+ * Total Submissions: 60.6K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
