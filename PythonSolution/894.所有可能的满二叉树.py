77e67d84a1a479d123d30f524d12a16c204bfa91
Gordon <gordondeng@foxmail.com>
2021-04-25 23:54:12 +0800

    Update 894.所有可能的满二叉树.py
    递归别想怎么构造，先想怎么递归

--- PythonSolution/894.所有可能的满二叉树.py
+++ PythonSolution/894.所有可能的满二叉树.py
@@ -49,7 +49,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# T:O(2^N) S:O(2^N)
+# https://leetcode-cn.com/problems/all-possible-full-binary-trees/solution/man-er-cha-shu-di-gui-xiang-jie-by-jalan/
 class Solution:
-    def allPossibleFBT(self, n: int) -> List[TreeNode]:
+    # 子问题：构造一棵满二叉树
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        res = []
+        if N == 1:
+            return [TreeNode(0)]
+        # 结点个数必须是奇数
+        if N % 2 == 0:
+            return []
+        
+        # 左子树分配一个节点
+        left_num = 1
+        # 右子树可以分配到 N - 1 - 1 = N - 2 个节点
+        right_num = N - 2
+        
+        while right_num > 0:
+            # 递归构造左子树
+            left_tree = self.allPossibleFBT(left_num)
+            # 递归构造右子树
+            right_tree = self.allPossibleFBT(right_num)
+            # 具体构造过程
+            for i in range(len(left_tree)):
+                for j in range(len(right_tree)):
+                    root = TreeNode(0)
+                    root.left = left_tree[i]
+                    root.right = right_tree[j]
+                    res.append(root)
+            left_num += 2
+            right_num -= 2
+        
+        return res  
 # @lc code=end
 
