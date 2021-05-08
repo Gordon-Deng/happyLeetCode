a781f3bbd0dc103c3dffba6ad79904479679e138
Gordon <gordondeng@foxmail.com>
2021-05-08 23:49:25 +0800

    Update 268.丢失的数字.py

--- PythonSolution/268.丢失的数字.py
+++ PythonSolution/268.丢失的数字.py
@@ -67,21 +67,14 @@
 #
 
 # @lc code=start
+
+# 位运算
+# T:O(N)) S:O(N)
 class Solution:
     def missingNumber(self, nums):
-        nums.sort()
-
-        # Ensure that n is at the last index
-        if nums[-1] != len(nums):
-            return len(nums)
-        # Ensure that 0 is at the first index
-        elif nums[0] != 0:
-            return 0
-
-        # If we get here, then the missing number is on the range (0, n)
-        for i in range(1, len(nums)):
-            expected_num = nums[i-1] + 1
-            if nums[i] != expected_num:
-                return expected_num
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
 # @lc code=end
 
