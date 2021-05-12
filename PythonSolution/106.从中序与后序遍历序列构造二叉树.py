fb658da28de59632245ebd251a84d54a4a6cbe5a
Gordon <gordondeng@foxmail.com>
2021-05-12 23:57:23 +0800

    Create 106.从中序与后序遍历序列构造二叉树.py

---
+++ PythonSolution/106.从中序与后序遍历序列构造二叉树.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=106 lang=python3
+#
+# [106] 从中序与后序遍历序列构造二叉树
+#
+# https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+#
+# algorithms
+# Medium (71.77%)
+# Likes:    498
+# Dislikes: 0
+# Total Accepted:    106.2K
+# Total Submissions: 147.9K
+# Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+#
+# 根据一棵树的中序遍历与后序遍历构造二叉树。
+# 
+# 注意:
+# 你可以假设树中没有重复的元素。
+# 
+# 例如，给出
+# 
+# 中序遍历 inorder = [9,3,15,20,7]
+# 后序遍历 postorder = [9,15,7,20,3]
+# 
+# 返回如下的二叉树：
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+# @lc code=end
+
