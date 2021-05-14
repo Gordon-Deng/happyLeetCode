6b57b076db3f8641e84a4c2bb265746415f7d756
Gordon <gordondeng@foxmail.com>
2021-05-15 01:21:24 +0800

    Update 814.二叉树剪枝.py

--- PythonSolution/814.二叉树剪枝.py
+++ PythonSolution/814.二叉树剪枝.py
@@ -65,8 +65,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def pruneTree(self, root: TreeNode) -> TreeNode:
-        
+
+# 普通减枝
+class Solution(object):
+    def pruneTree(self, root):
+        def containsOne(node):
+            if not node: return False
+            a1 = containsOne(node.left)
+            a2 = containsOne(node.right)
+            if not a1: node.left = None
+            if not a2: node.right = None
+            return node.val == 1 or a1 or a2
+
+        return root if containsOne(root) else None
 # @lc code=end
 
