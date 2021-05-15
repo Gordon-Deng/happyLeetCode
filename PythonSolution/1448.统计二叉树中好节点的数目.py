7c13afd10ceb0c19adb64dd50b9890bf8f07af98
Gordon <gordondeng@foxmail.com>
2021-05-16 00:24:50 +0800

    Update 1448.统计二叉树中好节点的数目.py

--- PythonSolution/1448.统计二叉树中好节点的数目.py
+++ PythonSolution/1448.统计二叉树中好节点的数目.py
@@ -64,7 +64,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.counter = 0
+
     def goodNodes(self, root: TreeNode) -> int:
-        
+        def dfs(node:TreeNode, max_value:int) -> int:
+            if not node: return
+            else:
+                # 如果存在比当前值大的计数器+1
+                if node.val>=max_value:
+                    self.counter += 1
+                    max_value=node.val
+                # 递归到叶子为止
+                dfs(node.left, max_value)
+                dfs(node.right, max_value)
+        # 最小值切入
+        dfs(root, float('-inf'))
+        return self.counter
 # @lc code=end
 
