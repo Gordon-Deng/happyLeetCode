dbd9e244b55b2efce59d12f0647c8580e54a4a93
Gordon <gordondeng@foxmail.com>
2021-05-18 00:09:38 +0800

    Update 105.从前序与中序遍历序列构造二叉树.py

--- PythonSolution/105.从前序与中序遍历序列构造二叉树.py
+++ PythonSolution/105.从前序与中序遍历序列构造二叉树.py
@@ -40,7 +40,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# T:O(N) S:O(N)
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder or not inorder:  # 递归终止条件
+            return
+        root = TreeNode(preorder[0])  # 先序为“根左右”，所以根据preorder可以确定root
+        idx = inorder.index(preorder[0])  # 中序为“左根右”，根据root可以划分出左右子树
+        # 下面递归对root的左右子树求解即可
+        root.left = self.buildTree(preorder[1:1 + idx], inorder[:idx])
+        root.right = self.buildTree(preorder[1 + idx:], inorder[idx + 1:])
+        return root
 # @lc code=end
 
