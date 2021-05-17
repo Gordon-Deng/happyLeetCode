06c6aade2f31f7bd39a0e9f814bcc092f7880af3
Gordon <gordondeng@foxmail.com>
2021-05-18 01:07:00 +0800

    Create 144.二叉树的前序遍历.py

---
+++ PythonSolution/144.二叉树的前序遍历.py
@@ -0,0 +1,93 @@
+#
+# @lc app=leetcode.cn id=144 lang=python3
+#
+# [144] 二叉树的前序遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+#
+# algorithms
+# Medium (69.71%)
+# Likes:    568
+# Dislikes: 0
+# Total Accepted:    318.3K
+# Total Submissions: 456.5K
+# Testcase Example:  '[1,null,2,3]'
+#
+# 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：root = [1,null,2,3]
+# 输出：[1,2,3]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：root = []
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：root = [1]
+# 输出：[1]
+# 
+# 
+# 示例 4：
+# 
+# 
+# 输入：root = [1,2]
+# 输出：[1,2]
+# 
+# 
+# 示例 5：
+# 
+# 
+# 输入：root = [1,null,2]
+# 输出：[1,2]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 树中节点数目在范围 [0, 100] 内
+# -100 
+# 
+# 
+# 
+# 
+# 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        res = []
+        stack = [(WHITE, root)]
+        while stack:
+            color, node = stack.pop()
+            if node is None: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((WHITE, node.left))
+                stack.append((GRAY, node))
+            else:
+                res.append(node.val)
+        return res
+# @lc code=end
+
