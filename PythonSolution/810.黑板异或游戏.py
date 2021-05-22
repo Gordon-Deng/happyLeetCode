c4a5970cd4ec12c6b5b581549d905a24cdf6e87f
Gordon <gordondeng@foxmail.com>
2021-05-23 00:22:42 +0800

    Update 810.黑板异或游戏.py

--- PythonSolution/810.黑板异或游戏.py
+++ PythonSolution/810.黑板异或游戏.py
@@ -49,6 +49,10 @@
 # @lc code=start
 class Solution:
     def xorGame(self, nums: List[int]) -> bool:
+        if len(nums) % 2 == 0:
+            return True
         
+        xorsum = reduce(xor, nums)
+        return xorsum == 0
 # @lc code=end
 
