ff60ee4d68915bc9a99b1ca3e8e1c70fd6578330
Gordon <gordondeng@foxmail.com>
2021-05-26 23:29:18 +0800

    Update 322.零钱兑换.py

--- PythonSolution/322.零钱兑换.py
+++ PythonSolution/322.零钱兑换.py
@@ -74,9 +74,9 @@ class Solution:
         
         for coin in coins:
             for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
-        return dp[amount] 
-        
-        if dp[amount] != float('inf') else -1 
+                if(x >= coin) {
+                    dp[x] = min(dp[x], dp[x - coin] + 1)
+                }
+        return dp[amount] if dp[amount] != float('inf') else -1 
 # @lc code=end
 
