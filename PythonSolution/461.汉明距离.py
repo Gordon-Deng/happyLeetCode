e54d845e36bd25132321491c7058a50403351077
Gordon <gordondeng@foxmail.com>
2021-05-27 23:37:56 +0800

    Create 461.汉明距离.py

---
+++ PythonSolution/461.汉明距离.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=461 lang=python3
+#
+# [461] 汉明距离
+#
+# https://leetcode-cn.com/problems/hamming-distance/description/
+#
+# algorithms
+# Easy (79.38%)
+# Likes:    471
+# Dislikes: 0
+# Total Accepted:    144.6K
+# Total Submissions: 179K
+# Testcase Example:  '1\n4'
+#
+# 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+# 
+# 给出两个整数 x 和 y，计算它们之间的汉明距离。
+# 
+# 注意：
+# 0 ≤ x, y < 2^31.
+# 
+# 示例:
+# 
+# 
+# 输入: x = 1, y = 4
+# 
+# 输出: 2
+# 
+# 解释:
+# 1   (0 0 0 1)
+# 4   (0 1 0 0)
+# ⁠      ↑   ↑
+# 
+# 上面的箭头指出了对应二进制位不同的位置。
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ret = 0
+        s = x ^ y
+        while s :
+            s  &= s - 1
+            ret +1
+        return ret
+# @lc code=end
+
