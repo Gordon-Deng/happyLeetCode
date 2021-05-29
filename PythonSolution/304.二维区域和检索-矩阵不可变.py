67945a657d66776494f9b3d29a28ad02b3e01930
Gordon <gordondeng@foxmail.com>
2021-05-30 02:53:50 +0800

    Create 304.二维区域和检索-矩阵不可变.py

---
+++ PythonSolution/304.二维区域和检索-矩阵不可变.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode.cn id=304 lang=python3
+#
+# [304] 二维区域和检索 - 矩阵不可变
+#
+# https://leetcode-cn.com/problems/range-sum-query-2d-immutable/description/
+#
+# algorithms
+# Medium (54.37%)
+# Likes:    270
+# Dislikes: 0
+# Total Accepted:    54.5K
+# Total Submissions: 100.7K
+# Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n' +
+  '[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
+#
+# 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+# 
+# 
+# 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+# 
+# 
+# 
+# 示例：
+# 
+# 
+# 给定 matrix = [
+# ⁠ [3, 0, 1, 4, 2],
+# ⁠ [5, 6, 3, 2, 1],
+# ⁠ [1, 2, 0, 1, 5],
+# ⁠ [4, 1, 0, 1, 7],
+# ⁠ [1, 0, 3, 0, 5]
+# ]
+# 
+# sumRegion(2, 1, 4, 3) -> 8
+# sumRegion(1, 1, 2, 2) -> 11
+# sumRegion(1, 2, 2, 4) -> 12
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 你可以假设矩阵不可变。
+# 会多次调用 sumRegion 方法。
+# 你可以假设 row1 ≤ row2 且 col1 ≤ col2 。
+# 
+# 
+#
+
+# @lc code=start
+# h = "变量1" if a>b else "变量2"
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        m, n = len(matrix), (len(matrix[0]) if matrix else 0)
+        self.sums = [[0] * (n + 1) for _ in range(m)]
+        _sums = self.sums
+
+        for i in range(m):
+            for j in range(n):
+                _sums[i][j + 1] = _sums[i][j] + matrix[i][j]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        _sums = self.sums
+
+        total = sum(_sums[i][col2 + 1] - _sums[i][col1] for i in range(row1, row2 + 1))
+        return total
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+# @lc code=end
+
