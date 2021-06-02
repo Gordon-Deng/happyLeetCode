18adf3a92d7b505bffac87705895ff4ccd0758e0
Gordon <gordondeng@foxmail.com>
2021-06-03 01:28:44 +0800

    Update 42.接雨水.py

--- PythonSolution/42.接雨水.py
+++ PythonSolution/42.接雨水.py
@@ -30,6 +30,20 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
+        ans = 0
+        left, right = 0, len(height) - 1
+        leftMax = rightMax = 0
+
+        while left < right:
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+            if height[left] < height[right]:
+                ans += leftMax - height[left]
+                left += 1
+            else:
+                ans += rightMax - height[right]
+                right -= 1
         
+        return ans     
 # @lc code=end
 
