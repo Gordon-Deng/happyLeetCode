2ca48aeeb689311921d3d27ffeca652deff2c207
Gordon <gordondeng@foxmail.com>
2021-06-05 22:54:38 +0800

    Create 203.移除链表元素.py

---
+++ PythonSolution/203.移除链表元素.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode.cn id=203 lang=python3
+#
+# [203] 移除链表元素
+#
+# https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+#
+# algorithms
+# Easy (48.36%)
+# Likes:    638
+# Dislikes: 0
+# Total Accepted:    185.7K
+# Total Submissions: 370.7K
+# Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+#
+# 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：head = [1,2,6,3,4,5,6], val = 6
+# 输出：[1,2,3,4,5]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：head = [], val = 1
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：head = [7,7,7,7], val = 7
+# 输出：[]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 列表中的节点在范围 [0, 10^4] 内
+# 1 
+# 0 
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode()
+        dummy.next = head
+        p = head
+        while p is not None:
+            # 向前探一个节点检查是否等于val
+            if p.next and p.next.val == val:
+                # 跳过 p.next 节点
+                p.next = p.next.next 
+            else:
+                p = p.next
+        return dummy.next  
+# @lc code=end
+
