3e7663e3a448ff12a229f7de6596713d8dd42375
Gordon <gordondeng@foxmail.com>
2021-06-08 22:58:53 +0800

    Update 1049.最后一块石头的重量-ii.py

--- PythonSolution/1049.最后一块石头的重量-ii.py
+++ PythonSolution/1049.最后一块石头的重量-ii.py
@@ -64,7 +64,15 @@
 #
 
 # @lc code=start
+
+# 精简做法
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        sums=sum(stones)
+        dp=[0]*(sums//2+1)
+        for x in stones:
+            for i in range(sums//2,x-1,-1):
+                dp[i]=max(dp[i],dp[i-x]+x)
+        return sums-2*dp[sums//2]
 # @lc code=end
 
