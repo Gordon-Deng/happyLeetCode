17e30038ee9e8abbc08ac7cc4e347f688c28007b
Gordon <gordondeng@foxmail.com>
2021-06-16 23:41:40 +0800

    Update 416.分割等和子集.py

--- PythonSolution/416.分割等和子集.py
+++ PythonSolution/416.分割等和子集.py
@@ -46,5 +46,17 @@
 # @lc code=start
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        sumAll = sum(nums)
+        if sumAll % 2:
+            return False
+        target = sumAll // 2
+
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for i in range(len(nums)):
+            for j in range(target, nums[i] - 1, -1):
+                dp[j] = dp[j] or dp[j - nums[i]]
+        return dp[-1]
 # @lc code=end
 
