358cb7457296d4230ef356f1d863eb864ba6c90d
Gordon <gordondeng@foxmail.com>
2021-06-20 00:04:45 +0800

    Update 1600.皇位继承顺序.py

--- PythonSolution/1600.皇位继承顺序.py
+++ PythonSolution/1600.皇位继承顺序.py
@@ -104,34 +104,5 @@
 #
 
 # @lc code=start
-class ThroneInheritance:
-
-    def __init__(self, kingName: str):
-        self.king = kingName
-        self.children = defaultdict(list)
-        self.dead_set = set()
-
-    def birth(self, parentName: str, childName: str) -> None:
-        self.children[parentName].append(childName)
-
-    def death(self, name: str) -> None:
-        self.dead_set.add(name)
-
-    def getInheritanceOrder(self) -> List[str]:
-        self.res = []
-
-        def dfs_NLR(king: str) -> None:         #N叉树的，前序遍历
-            if king not in self.dead_set:
-                self.res.append(king)
-            for child in self.children[king]:
-                dfs_NLR(child)
-
-        dfs_NLR(self.king)
-        return self.res
-# Your ThroneInheritance object will be instantiated and called as such:
-# obj = ThroneInheritance(kingName)
-# obj.birth(parentName,childName)
-# obj.death(name)
-# param_3 = obj.getInheritanceOrder()
 # @lc code=end
 
