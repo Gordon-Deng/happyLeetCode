4cca69287cb7a5ae14c2c4b0141ddb5e22cb276e
Gordon <gordondeng@foxmail.com>
2021-06-23 23:42:17 +0800

    Create 32.最长有效括号.py

---
+++ PythonSolution/32.最长有效括号.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode.cn id=32 lang=python3
+#
+# [32] 最长有效括号
+#
+# https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+#
+# algorithms
+# Hard (35.09%)
+# Likes:    1347
+# Dislikes: 0
+# Total Accepted:    159.5K
+# Total Submissions: 454.1K
+# Testcase Example:  '"(()"'
+#
+# 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+# 
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：s = "(()"
+# 输出：2
+# 解释：最长有效括号子串是 "()"
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：s = ")()())"
+# 输出：4
+# 解释：最长有效括号子串是 "()()"
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：s = ""
+# 输出：0
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 
+# s[i] 为 '(' 或 ')'
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+# @lc code=end
+
