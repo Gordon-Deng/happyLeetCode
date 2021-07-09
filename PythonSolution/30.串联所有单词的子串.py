3da707a3ae9af9a00fc9076dd783dbac0811ae8f
Gordon <gordondeng@foxmail.com>
2021-07-09 23:46:11 +0800

    Update 30.串联所有单词的子串.py

--- PythonSolution/30.串联所有单词的子串.py
+++ PythonSolution/30.串联所有单词的子串.py
@@ -63,17 +63,27 @@ class Solution:
         from collections import Counter
         if not s or not words:return []
         one_word = len(words[0])
-        all_len = len(words) * one_word
+        word_num = len(words)
         n = len(s)
         words = Counter(words)
         res = []
-        for i in range(0, n - all_len + 1):
-            tmp = s[i:i+all_len]
-            c_tmp = []
-            for j in range(0, all_len, one_word):
-                c_tmp.append(tmp[j:j+one_word])
-            if Counter(c_tmp) == words:
-                res.append(i)
+        for i in range(0, one_word):
+            cur_cnt = 0
+            left = i
+            right = i
+            cur_Counter = Counter()
+            while right + one_word <= n:
+                w = s[right:right + one_word]
+                right += one_word
+                cur_Counter[w] += 1
+                cur_cnt += 1
+                while cur_Counter[w] > words[w]:
+                    left_w = s[left:left+one_word]
+                    left += one_word
+                    cur_Counter[left_w] -= 1
+                    cur_cnt -= 1
+                if cur_cnt == word_num :
+                    res.append(left)
         return res
 # @lc code=end
 
