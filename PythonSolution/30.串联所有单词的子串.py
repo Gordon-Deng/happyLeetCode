0cd9de69da1911ba4240d54776a7146b0cfb9f3f
Gordon <gordondeng@foxmail.com>
2021-07-09 23:45:34 +0800

    Update 30.串联所有单词的子串.py

--- PythonSolution/30.串联所有单词的子串.py
+++ PythonSolution/30.串联所有单词的子串.py
@@ -60,5 +60,21 @@
 # @lc code=start
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        from collections import Counter
+        if not s or not words:return []
+        all_len = sum(map(len, words))
+        n = len(s)
+        words = Counter(words)
+        res = []
+        for i in range(0, n - all_len + 1):
+            tmp = s[i:i+all_len]
+            flag = True
+            for key in words:
+                if words[key] != tmp.count(key):
+                    flag = False
+                    break
+            if flag:res.append(i)
+        return res
+
 # @lc code=end
 
