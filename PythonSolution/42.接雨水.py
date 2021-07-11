605e1c44041f4a25d616bec4fabc14662c4ec4ac
Gordon <gordondeng@foxmail.com>
2021-07-11 23:40:35 +0800

    Update 42.接雨水.py
    双指针

--- PythonSolution/42.接雨水.py
+++ PythonSolution/42.接雨水.py
@@ -65,26 +65,50 @@
 
 # DP
 # T:O(N) S:O(N)
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         # 边界条件
+#         if not height: return 0
+#         n = len(height)
+#         maxleft = [0] * n
+#         maxright = [0] * n
+#         ans = 0
+#         # 初始化
+#         maxleft[0] = height[0]
+#         maxright[n-1] = height[n-1]
+#         # 设置备忘录，分别存储左边和右边最高的柱子高度
+#         for i in range(1,n):
+#             maxleft[i] = max(height[i],maxleft[i-1])
+#         for j in range(n-2,-1,-1):
+#             maxright[j] = max(height[j],maxright[j+1])
+#         # 一趟遍历，比较每个位置可以存储多少水
+#         for i in range(n):
+#             if min(maxleft[i],maxright[i]) > height[i]:
+#                 ans += min(maxleft[i],maxright[i]) - height[i]
+#         return ans
+
+# 双指针
+# T:O(N) S:O(1)
 class Solution:
     def trap(self, height: List[int]) -> int:
         # 边界条件
         if not height: return 0
         n = len(height)
-        maxleft = [0] * n
-        maxright = [0] * n
+
+        left,right = 0, n - 1  # 分别位于输入数组的两端
+        maxleft,maxright = height[0],height[n - 1]
         ans = 0
-        # 初始化
-        maxleft[0] = height[0]
-        maxright[n-1] = height[n-1]
-        # 设置备忘录，分别存储左边和右边最高的柱子高度
-        for i in range(1,n):
-            maxleft[i] = max(height[i],maxleft[i-1])
-        for j in range(n-2,-1,-1):
-            maxright[j] = max(height[j],maxright[j+1])
-        # 一趟遍历，比较每个位置可以存储多少水
-        for i in range(n):
-            if min(maxleft[i],maxright[i]) > height[i]:
-                ans += min(maxleft[i],maxright[i]) - height[i]
+
+        while left <= right:
+            maxleft = max(height[left],maxleft)
+            maxright = max(height[right],maxright)
+            if maxleft < maxright:
+                ans += maxleft - height[left]
+                left += 1
+            else:
+                ans += maxright - height[right]
+                right -= 1
+
         return ans
 # @lc code=end
 
