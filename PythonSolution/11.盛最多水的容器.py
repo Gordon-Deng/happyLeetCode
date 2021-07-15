03d8a6f0ec647876a9116a5d53c123bd73851aa3
Gordon <gordondeng@foxmail.com>
2021-07-15 22:37:32 +0800

    Update 11.盛最多水的容器.py

--- PythonSolution/11.盛最多水的容器.py
+++ PythonSolution/11.盛最多水的容器.py
@@ -34,5 +34,14 @@
 # @lc code=start
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        i, j, res = 0, len(height) - 1, 0
+        while i < j:
+            if height[i] < height[j]:
+                res = max(res, height[i] * (j - i))
+                i += 1
+            else:
+                res = max(res, height[j] * (j - i))
+                j -= 1
+        return res
 # @lc code=end
 
