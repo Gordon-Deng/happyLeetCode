c1e21e8b16bac62433c9d3600f75685ceb00fc68
Gordon <gordondeng@foxmail.com>
2021-07-25 23:57:40 +0800

    Create 155.最小栈.py

---
+++ PythonSolution/155.最小栈.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode.cn id=155 lang=python3
+#
+# [155] 最小栈
+#
+# https://leetcode-cn.com/problems/min-stack/description/
+#
+# algorithms
+# Easy (50.89%)
+# Likes:    383
+# Dislikes: 0
+# Total Accepted:    73.4K
+# Total Submissions: 142.4K
+# Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+#
+# 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+# 
+# 
+# push(x) -- 将元素 x 推入栈中。
+# pop() -- 删除栈顶的元素。
+# top() -- 获取栈顶元素。
+# getMin() -- 检索栈中的最小元素。
+# 
+# 
+# 示例:
+# 
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> 返回 -3.
+# minStack.pop();
+# minStack.top();      --> 返回 0.
+# minStack.getMin();   --> 返回 -2.
+# 
+# 
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def push(self, x: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
