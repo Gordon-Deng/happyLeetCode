9c0dc09239793c8f1812fac26a12e8de62cb2ebe
Gordon <gordondeng@foxmail.com>
2021-07-27 02:03:45 +0800

    Update 155.最小栈.py

--- PythonSolution/155.最小栈.py
+++ PythonSolution/155.最小栈.py
@@ -38,46 +38,26 @@
 #
 
 # @lc code=start
-class MinStack(object):
-
+class MinStack:
+    
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stack = []
-        
+        self.min_stack = []
 
-    def push(self, x):
-        """
-        :type x: int
-        :rtype: void
-        """
-        if not self.stack:
-            self.stack.append((x, x))
-        else:
-            self.stack.append((x, min(x, self.stack[-1][1])))
-        
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]: 
+            self.min_stack.append(x)
 
-    def pop(self):
-        """
-        :rtype: void
-        """
-        self.stack.pop()
-        
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
 
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.stack[-1][0]
-        
+    def top(self) -> int:
+        return self.stack[-1]
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return self.stack[-1][1]
-
+    def getMin(self) -> int:
+        return self.min_stack[-1]
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(x)
