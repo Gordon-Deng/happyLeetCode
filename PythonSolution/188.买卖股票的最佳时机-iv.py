682edb3f414725a4aa5a350c784a60a28a290116
Gordon <gordondeng@foxmail.com>
2021-08-01 20:19:00 +0800

    add

--- PythonSolution/188.买卖股票的最佳时机-iv.py
+++ PythonSolution/188.买卖股票的最佳时机-iv.py
@@ -52,5 +52,16 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        k = min(k, len(prices) // 2)
+
+        buy = [-float("inf")] * (k+1)
+        sell = [0] * (k+1)
+
+        for p in prices:
+            for i in range(1, k+1):
+                buy[i] = max(buy[i], sell[i-1] - p)
+                sell[i] = max(sell[i], buy[i] + p)
+
+        return sell[-1]
 # @lc code=end
 
