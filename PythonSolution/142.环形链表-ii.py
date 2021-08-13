63fa0ff3bb801a8e6288ef2c936ef7273c4765fd
Gordon <gordondeng@foxmail.com>
2021-08-14 01:34:22 +0800

    1

--- PythonSolution/142.环形链表-ii.py
+++ PythonSolution/142.环形链表-ii.py
@@ -69,7 +69,6 @@ class Solution:
             if not (fast and fast.next): return
             fast, slow = fast.next.next, slow.next
             if fast == slow: break
-            
         fast = head
         while fast != slow:
             fast, slow = fast.next, slow.next
--- PythonSolution/88.合并两个有序数组.py
+++ PythonSolution/88.合并两个有序数组.py
@@ -35,24 +35,12 @@
 # @lc code=start
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        p1, p2 = m - 1, n - 1
-        tail = m + n - 1
-        while p1 >= 0 or p2 >= 0:
-            if p1 == -1:
-                nums1[tail] = nums2[p2]
-                p2 -= 1
-            elif p2 == -1:
-                nums1[tail] = nums1[p1]
-                p1 -= 1
-            elif nums1[p1] > nums2[p2]:
-                nums1[tail] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[tail] = nums2[p2]
-                p2 -= 1
-            tail -= 1
+        if not nums1 or not nums2: return 
+        p, q = m-1, n-1
+        while p >0 or q > 0:
+            if  p == 0:
+                continue
+            if q == 0:
+                continue
 # @lc code=end
 
