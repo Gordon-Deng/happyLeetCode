b3366101097c2a6fbc3e4919374c97840fd68b5f
Gordon <gordondeng@foxmail.com>
2021-08-16 23:57:17 +0800

    Create 25.k-个一组翻转链表.py

---
+++ PythonSolution/25.k-个一组翻转链表.py
@@ -0,0 +1,113 @@
+#
+# @lc app=leetcode.cn id=25 lang=python3
+#
+# [25] K 个一组翻转链表
+#
+# https://leetcode-cn.com/problems/reverse-nodes-in-k-group/description/
+#
+# algorithms
+# Hard (65.44%)
+# Likes:    1242
+# Dislikes: 0
+# Total Accepted:    209.1K
+# Total Submissions: 319.2K
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+# 
+# k 是一个正整数，它的值小于或等于链表的长度。
+# 
+# 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+# 
+# 进阶：
+# 
+# 
+# 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+# 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：head = [1,2,3,4,5], k = 2
+# 输出：[2,1,4,3,5]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：head = [1,2,3,4,5], k = 3
+# 输出：[3,2,1,4,5]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：head = [1,2,3,4,5], k = 1
+# 输出：[1,2,3,4,5]
+# 
+# 
+# 示例 4：
+# 
+# 
+# 输入：head = [1], k = 1
+# 输出：[1]
+# 
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 列表中节点的数量在范围 sz 内
+# 1 
+# 0 
+# 1 
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # 翻转一个子链表，并且返回新的头与尾
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+
+        while head:
+            tail = pre
+            # 查看剩余部分长度是否大于等于 k
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+            # 把子链表重新接回原链表
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+        
+        return hair.next
+# @lc code=end
+
