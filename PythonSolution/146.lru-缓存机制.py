b7c84287e7c37038bdfec3e42e830089a0ae668f
Gordon <gordondeng@foxmail.com>
2021-08-20 02:26:21 +0800

    Update 146.lru-缓存机制.py

--- PythonSolution/146.lru-缓存机制.py
+++ PythonSolution/146.lru-缓存机制.py
@@ -43,71 +43,64 @@
 
 # @lc code=start
 # python 入参可以为0
-class DLinkedNode:
+class DlinkedNode:
     def __init__(self, key=0, value=0):
         self.key = key
         self.value = value
-        self.prev = None
+        self.pre = None
         self.next = None
-
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cache = dict()
-        # 使用伪头部和伪尾部节点    
-        self.head = DLinkedNode()
-        self.tail = DLinkedNode()
+        self.head = DlinkedNode()
+        self.tail = DlinkedNode()
+        self.tail.pre = self.head
         self.head.next = self.tail
-        self.tail.prev = self.head
-        self.capacity = capacity
         self.size = 0
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
+        # 有的话返回，置顶
         if key not in self.cache:
-            return -1
-        # 如果 key 存在，先通过哈希表定位，再移到头部
+            return -1 
         node = self.cache[key]
-        self.moveToHead(node)
+        self.moveToTop(node)
         return node.value
 
     def put(self, key: int, value: int) -> None:
         if key not in self.cache:
-            # 如果 key 不存在，创建一个新的节点
-            node = DLinkedNode(key, value)
-            # 添加进哈希表
-            self.cache[key] = node
-            # 添加至双向链表的头部
+            node = DlinkedNode(key, value)
+            self.cache[node.key] = node
             self.addToHead(node)
             self.size += 1
             if self.size > self.capacity:
-                # 如果超出容量，删除双向链表的尾部节点
                 removed = self.removeTail()
-                # 删除哈希表中对应的项
                 self.cache.pop(removed.key)
-                self.size -= 1
+                self.size -=1
         else:
-            # 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
             node = self.cache[key]
             node.value = value
-            self.moveToHead(node)
-    
-    def addToHead(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
-    
-    def removeNode(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+            self.moveToTop(node)
 
-    def moveToHead(self, node):
+
+    def moveToTop(self, node: DlinkedNode):
         self.removeNode(node)
         self.addToHead(node)
 
+    def removeNode(self, node: DlinkedNode):
+        node.pre.next = node.next
+        node.next.pre = node.pre
+
+    def addToHead(self, node: DlinkedNode):
+        node.pre = self.head
+        node.next = self.head.next
+        self.head.next.pre = node
+        self.head.next = node
+
     def removeTail(self):
-        node = self.tail.prev
+        node = self.tail.pre
         self.removeNode(node)
         return node
 # Your LRUCache object will be instantiated and called as such:
