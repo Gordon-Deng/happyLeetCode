a9386abccad1872736d368ddfdb8b784659d63d5
Gordon <gordondeng@foxmail.com>
2021-08-31 23:17:41 +0800

    Update 215.数组中的第k个最大元素.py

--- PythonSolution/215.数组中的第k个最大元素.py
+++ PythonSolution/215.数组中的第k个最大元素.py
@@ -33,13 +33,72 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         heap = list()
+#         for num in nums:
+#             heapq.heappush(heap, num)
+#             if len(heap) > k:
+#                 heapq.heappop(heap)
+#         return heap[0]
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = list()
+        heap = Heap(k + 1)
         for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return heap[0]
+            if not heap.push(num):
+                heap.pop()
+                heap.push(num)
+        if heap.size == k + 1:
+            heap.pop()
+        return heap.peek()
+        
+class Heap:
+    def __init__(self, length):
+        self.heap = [0] * (length + 1)
+        self.size = 0
+    
+    def push(self, val):
+        if self.size == len(self.heap) - 1:
+            return False
+        self.size += 1
+        self.heap[self.size] = val
+        self.shift_up(self.size)
+        return True
+    
+    def pop(self):
+        val = self.heap[1]
+        self.heap[1] = self.heap[self.size]
+        self.heap[self.size] = 0
+        self.size -= 1
+        self.shift_down(1)
+        return val
+    
+    def peek(self):
+        return self.heap[1]
+    
+    def shift_up(self, i):
+        val = self.heap[i]
+        while i >> 1 > 0:
+            parent = i >> 1
+            if val < self.heap[parent]:
+                self.heap[i] = self.heap[parent]
+                i = parent
+            else:
+                break
+        self.heap[i] = val
+    
+    def shift_down(self, i):
+        val = self.heap[i]
+        while i << 1 <= self.size:
+            child = i << 1
+            if child != self.size and self.heap[child + 1] < self.heap[child]:
+                child += 1
+            if val > self.heap[child]:
+                self.heap[i] = self.heap[child]
+                i = child
+            else:
+                break
+        self.heap[i] = val
 # @lc code=end
 
