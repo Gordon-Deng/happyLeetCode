3d69cc0e72c9c4484cf72c63eef111452bfb0a66
Gordon <gordondeng@foxmail.com>
2021-09-02 01:17:12 +0800

    Update 53.最大子序和.py

--- PythonSolution/53.最大子序和.py
+++ PythonSolution/53.最大子序和.py
@@ -31,6 +31,17 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
+        size = len(nums)
+        if size == 0:
+            return 0
+        dp = [0 for _ in range(size)]
+
+        dp[0] = nums[0]
+        for i in range(1, size):
+            if dp[i - 1] >= 0:
+                dp[i] = dp[i - 1] + nums[i]
+            else:
+                dp[i] = nums[i]
+        return max(dp)
 # @lc code=end
 
