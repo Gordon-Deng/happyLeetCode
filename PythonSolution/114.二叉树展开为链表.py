6cf4108e8fd2a2d43537e9809e4e9765ee48b88b
Gordon <gordondeng@foxmail.com>
2021-09-05 03:09:16 +0800

    Update 114.二叉树展开为链表.py

--- PythonSolution/114.二叉树展开为链表.py
+++ PythonSolution/114.二叉树展开为链表.py
@@ -79,5 +79,23 @@ class Solution:
                 curr.left = None
                 curr.right = nxt
             curr = curr.right
+
+# 低端版
+# class Solution:
+#     def flatten(self, root: TreeNode) -> None:
+#         preorderList = list()
+
+#         def preorderTraversal(root: TreeNode):
+#             if root:
+#                 preorderList.append(root)
+#                 preorderTraversal(root.left)
+#                 preorderTraversal(root.right)
+        
+#         preorderTraversal(root)
+#         size = len(preorderList)
+#         for i in range(1, size):
+#             prev, curr = preorderList[i - 1], preorderList[i]
+#             prev.left = None
+#             prev.right = curr
 # @lc code=end
 
