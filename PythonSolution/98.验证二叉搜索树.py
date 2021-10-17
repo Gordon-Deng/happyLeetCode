264d1730aa7831a8d8aa6645350d2f120be03fa4
Gordon <gordondeng@foxmail.com>
2021-10-18 02:32:35 +0800

    Create 98.验证二叉搜索树.py

---
+++ PythonSolution/98.验证二叉搜索树.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode.cn id=98 lang=python3
+#
+# [98] 验证二叉搜索树
+#
+# https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+#
+# algorithms
+# Medium (35.00%)
+# Likes:    1253
+# Dislikes: 0
+# Total Accepted:    356.5K
+# Total Submissions: 1M
+# Testcase Example:  '[2,1,3]'
+#
+# 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+# 
+# 有效 二叉搜索树定义如下：
+# 
+# 
+# 节点的左子树只包含 小于 当前节点的数。
+# 节点的右子树只包含 大于 当前节点的数。
+# 所有左子树和右子树自身必须也是二叉搜索树。
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：root = [2,1,3]
+# 输出：true
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：root = [5,1,4,null,null,3,6]
+# 输出：false
+# 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 树中节点数目范围在[1, 10^4] 内
+# -2^31 <= Node.val <= 2^31 - 1
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        self.flag = True
+        def findTop(target, root) :
+            if not root:
+                return -float("inf")
+            left = findTop(target, root.left)
+            right = findTop(target, root.right)
+            if left > root.val or right < root.val:
+                self.flag = False
+            return max(target, max(left, right))
+        
+        left = findTop(root.val, root.left)
+        right = findTop(root.val, root.right)
+        return left <= root.val and right >= root.val and self.flag
+# @lc code=end
+
