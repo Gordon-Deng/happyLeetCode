42fa178c579f1f4deebafeed3e015bad5c75f211
Gordon <gordondeng@foxmail.com>
2021-10-23 23:54:16 +0800

    Update 3.无重复字符的最长子串.py

--- PythonSolution/3.无重复字符的最长子串.py
+++ PythonSolution/3.无重复字符的最长子串.py
@@ -1,25 +1,25 @@
 #
 # @lc app=leetcode.cn id=3 lang=python3
 #
-# [3] 无重复字符的最长子串
-#
-# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
-#
-# algorithms
-# Medium (32.44%)
-# Likes:    2984
-# Dislikes: 0
-# Total Accepted:    316.5K
-# Total Submissions: 975.2K
-# Testcase Example:  '"abcabcbb"'
-#
-# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-# 
-# 示例 1:
-# 
-# 输入: "abcabcbb"
-# 输出: 3 
-# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+[3] 无重复字符的最长子串
+
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+
+algorithms
+Medium (32.44%)
+Likes:    2984
+Dislikes: 0
+Total Accepted:    316.5K
+Total Submissions: 975.2K
+Testcase Example:  '"abcabcbb"'
+
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 # 
 # 
 # 示例 2:
@@ -42,21 +42,21 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s: return 0
-        cur_len = 0
-        max_len = 0
-        left = 0
+        # 哈希集合，记录每个字符是否出现过
+        occ = set()
         n = len(s)
-        flag = set()
-
+        # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        rk, ans = -1, 0
         for i in range(n):
-            cur_len += 1
-            while s[i] in flag:
-                flag.remove(s[left])
-                left += 1
-                cur_len -= 1
-            if cur_len > max_len:  max_len = cur_len
-            flag.add(s[i])
-        return max_len
+            if i != 0:
+                # 左指针向右移动一格，移除一个字符
+                occ.remove(s[i - 1])
+            while rk + 1 < n and s[rk + 1] not in occ:
+                # 不断地移动右指针
+                occ.add(s[rk + 1])
+                rk += 1
+            # 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1)
+        return ans
 # @lc code=end
 
