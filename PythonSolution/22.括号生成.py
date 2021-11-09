e77fb1d3328b7bdae520fa26a42f55560d8a05fc
Gordon <gordondeng@foxmail.com>
2021-11-09 23:52:17 +0800

    Update 22.括号生成.py

--- PythonSolution/22.括号生成.py
+++ PythonSolution/22.括号生成.py
@@ -29,21 +29,41 @@
 #
 
 # @lc code=start
-from typing import List
+# from typing import List
 
+
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         ans = []
+#         def backtrack(temp, left_bracket, right_bracket):
+#             if len(temp) == 2*n:
+#                 # list转string
+#                 ans.append("".join(temp))
+#                 return
+#             if left_bracket < n:
+#                 temp.append("(")
+#                 backtrack(temp, left_bracket+1, right_bracket)
+#                 temp.pop()
+#             if right_bracket < left_bracket:
+#                 temp.append(")")
+#                 backtrack(temp, left_bracket, right_bracket+1)
+#                 temp.pop()
+#         backtrack([], 0, 0)
+#         return ans
+
+from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
         def backtrack(temp, left_bracket, right_bracket):
             if len(temp) == 2*n:
-                # list转string
-                # ans.append("".join(temp))
                 ans.append(temp)
                 return
             if left_bracket < n:
                 temp = temp + "("
                 backtrack(temp, left_bracket+1, right_bracket)
+                # 消除末尾字符
                 temp = temp[:-1]
             if right_bracket < left_bracket:
                 temp = temp + ")"
