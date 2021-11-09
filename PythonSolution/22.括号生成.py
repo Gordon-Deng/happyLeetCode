18f70cbbc9ca22d78d011a5b84d0340e32fb59ef
Gordon <gordondeng@foxmail.com>
2021-11-09 23:09:03 +0800

    Create 22.括号生成.py

---
+++ PythonSolution/22.括号生成.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+# https://leetcode-cn.com/problems/generate-parentheses/description/
+#
+# algorithms
+# Medium (72.75%)
+# Likes:    758
+# Dislikes: 0
+# Total Accepted:    75.3K
+# Total Submissions: 102.7K
+# Testcase Example:  '3'
+#
+# 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+# 
+# 例如，给出 n = 3，生成结果为：
+# 
+# [
+# ⁠ "((()))",
+# ⁠ "(()())",
+# ⁠ "(())()",
+# ⁠ "()(())",
+# ⁠ "()()()"
+# ]
+# 
+# 
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        cur_str = ''
+
+        def dfs(cur_str, left, right):
+            """
+            :param cur_str: 从根结点到叶子结点的路径字符串
+            :param left: 左括号还可以使用的个数
+            :param right: 右括号还可以使用的个数
+            :return:
+            """
+            if left == 0 and right == 0:
+                res.append(cur_str)
+                return
+            if right < left:
+                return
+            if left > 0:
+                dfs(cur_str + '(', left - 1, right)
+            if right > 0:
+                dfs(cur_str + ')', left, right - 1)
+
+        dfs(cur_str, n, n)
+        return res   
+# @lc code=end
+
