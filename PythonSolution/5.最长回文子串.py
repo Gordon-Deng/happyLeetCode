9c53899c156250e9656da36fccca4dc38740232a
Gordon <gordondeng@foxmail.com>
2021-11-19 01:10:12 +0800

    add

--- PythonSolution/5.最长回文子串.py
+++ PythonSolution/5.最长回文子串.py
@@ -1,34 +1,34 @@
 #
 # @lc app=leetcode.cn id=5 lang=python3
 #
-# [5] 最长回文子串
-#
-# https://leetcode-cn.com/problems/longest-palindromic-substring/description/
-#
-# algorithms
-# Medium (28.04%)
-# Likes:    1622
-# Dislikes: 0
-# Total Accepted:    165.2K
-# Total Submissions: 588K
-# Testcase Example:  '"babad"'
-#
-# 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
-# 
-# 示例 1：
-# 
-# 输入: "babad"
-# 输出: "bab"
-# 注意: "aba" 也是一个有效答案。
-# 
-# 
-# 示例 2：
-# 
-# 输入: "cbbd"
-# 输出: "bb"
-# 
-# 
-#
+[5] 最长回文子串
+
+https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+
+algorithms
+Medium (28.04%)
+Likes:    1622
+Dislikes: 0
+Total Accepted:    165.2K
+Total Submissions: 588K
+Testcase Example:  '"babad"'
+
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+
+
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+
+
 
 # @lc code=start
 class Solution:
