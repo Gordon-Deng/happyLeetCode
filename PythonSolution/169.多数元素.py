6725d873a97f51476b33b42abc20c4c576ea0614
Gordon <gordondeng@foxmail.com>
2021-12-11 17:41:59 +0800

    update

--- PythonSolution/169.多数元素.py
+++ PythonSolution/169.多数元素.py
@@ -2,9 +2,9 @@
 # @lc app=leetcode.cn id=169 lang=python3
 #
 # [169] 多数元素
-#
+
 # https://leetcode-cn.com/problems/majority-element/description/
-#
+
 # algorithms
 # Easy (61.05%)
 # Likes:    445
@@ -12,18 +12,18 @@
 # Total Accepted:    110K
 # Total Submissions: 178.5K
 # Testcase Example:  '[3,2,3]'
-#
+
 # 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
-# 
+
 # 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
-# 
+
 # 示例 1:
-# 
+
 # 输入: [3,2,3]
 # 输出: 3
-# 
+
 # 示例 2:
-# 
+
 # 输入: [2,2,1,1,1,2,2]
 # 输出: 2
 # 
