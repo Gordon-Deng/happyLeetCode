a64d509ef2a2053b344a72c30725ed17a2304bfd
Gordon <gordondeng@foxmail.com>
2021-12-14 00:15:38 +0800

    Update 2105.【周赛271】给植物浇水2.py

--- PythonSolution/2105.【周赛271】给植物浇水2.py
+++ PythonSolution/2105.【周赛271】给植物浇水2.py
@@ -71,29 +71,25 @@
 # 双指针模拟
 class Solution:
     def minimumRefill(self, plants: List[int], capacityA: int, capacityB: int) -> int:
-        res = 0   # 灌满水罐次数
         n = len(plants)
-        posa, posb = 0, n - 1   # 两人位置
-        vala, valb = capacityA, capacityB   # 两人剩余水量
-        # 模拟相遇前的浇水过程
-        while posa < posb:
-            if vala < plants[posa]:
+        
+        res = 0
+        l = 0
+        r = n - 1
+        A = capacityA
+        B = capacityB
+        while l < r:
+            if A < plants[l]:
                 res += 1
-                vala = capacityA - plants[posa]
-            else:
-                vala -= plants[posa]
-            posa += 1
-            if valb < plants[posb]:
+                A = capacityA
+            if B < plants[r]:
                 res += 1
-                valb = capacityB - plants[posb]
-            else:
-                valb -= plants[posb]
-            posb -= 1
-        # 模拟相遇后可能的浇水过程
-        if posa == posb:
-            if vala >= valb and vala < plants[posa]:
-                res += 1
-            elif vala < valb and valb < plants[posb]:
+                B = capacityB
+            A -= plants[l]
+            B -= plants[r]
+            l += 1
+            r -= 1
+        if l == r:
+            if A < plants[l] and B < plants[r]:
                 res += 1
         return res
-
