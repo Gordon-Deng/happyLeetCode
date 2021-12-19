1801dbcf1c05c3474377e8c7f6387ab34cdfd7b5
Gordon <gordondeng@foxmail.com>
2021-12-20 01:21:20 +0800

    Update 5958.【周赛272】 股票平滑下跌阶段的数目.py

--- PythonSolution/5958.【周赛272】 股票平滑下跌阶段的数目.py
+++ PythonSolution/5958.【周赛272】 股票平滑下跌阶段的数目.py
@@ -1,13 +1,53 @@
 # https://leetcode-cn.com/problems/number-of-smooth-descent-periods-of-a-stock/
 
-    def addSpaces(self, s: str, spaces: List[int]) -> str:
-        ans = list()
+class Solution:
+	def find_desc_smoothly(nums: List[str]) -> bool:
 
-        ptr = 0
-        for i, ch in enumerate(s):
-            if ptr < len(spaces) and spaces[ptr] == i:
-                ans.append(" ")
-                ptr += 1
-            ans.append(ch)
-        
-        return "".join(ans)
+    def subsets_binary(nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        for state in range(0, 1 << n):
+            cur = []
+            for i in range(n):
+                if (state >> i) & 1:
+                    cur.append(nums[i])
+            res.append(cur)
+        return res
+
+    def subsets_dfs(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+
+        def dfs(idx: int, path: List[int]) -> None:
+            nonlocal res
+            if idx == n:
+                res.append(path)
+                return 
+            dfs(idx + 1, path + [nums[idx]])
+            dfs(idx + 1, path)
+
+        dfs(0, [])
+        return res
+
+    def subsets_recall(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+
+        path = []
+        def backtrace(idx: int) -> None:
+            nonlocal res
+            nonlocal path
+            if idx == n:
+                res.append(path[:])
+                return 
+                
+            backtrace(idx + 1)
+
+            path.append(nums[idx])
+            backtrace(idx + 1)
+            path.pop()
+
+        backtrace(0)
+        return res
+
+    def getDescentPeriods(self, prices: List[int]) -> int:
