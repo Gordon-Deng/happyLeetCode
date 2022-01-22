6038e0acb5253f743c97daab1940f5839304dbb7
Gordon <gordondeng@foxmail.com>
2022-01-22 23:55:25 +0800

    Update 1691.堆叠长方体的最大高度.py

--- PythonSolution/1691.堆叠长方体的最大高度.py
+++ PythonSolution/1691.堆叠长方体的最大高度.py
@@ -74,13 +74,18 @@
 class Solution:
     def maxHeight(self, cuboids: List[List[int]]) -> int:
         n = len(cuboids)
-        dp = [0] * n
-        cuboids.sort(key=lambda x:(x[0],-x[1]))
-        for i in range(n):
-            dp[i] = cuboids[i][2]
-            for j in range(i):
-                if all(cuboids[j][k] < cuboids[i][k] for k in range(1,3)):
-                    dp[i] = max(dp[i], dp[j] + cuboids[i][2])
+        for i in range(n):          #本题比较简单在于，长、宽、高3个维度，要弱就都弱，不管站着、平躺、侧躺，都弱，就是真的小
+            cuboids[i].sort()          #每个块从小到大排列  大的作为高
+        cuboids.sort(key = lambda x: (x[2], x[1], x[0]))   #按高度，从小到大排列
+
+        dp = [0 for _ in range(n)]
+        for R in range(n):          #dp时，后面的依赖前面的，故从前往后遍历
+            pre_max = 0             #跳出前面最大的情况
+            for L in range(R):
+                if cuboids[L][0] <= cuboids[R][0] and cuboids[L][1] <= cuboids[R][1]:   #前面的长和宽都小
+                    pre_max = max(pre_max, dp[L])
+            dp[R] = pre_max + cuboids[R][2]
+        
         return max(dp)
 # @lc code=end
 
